e540256f4b0843dc1ea79118ce71976f74e68829
Chunao Liu <787811283@qq.com>
2021-10-15 03:20:44 -0400

    Sprint Boot App Initialized

---
+++ BackEnd/HomieApp/HomieApp/src/main/java/Homie/HomieApp/HomieAppApplication.java
@@ -0,0 +1,13 @@
+package Homie.HomieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomieAppApplication.class, args);
+	}
+
+}
---
+++ BackEnd/HomieApp/HomieApp/src/test/java/Homie/HomieApp/HomieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Homie.HomieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
